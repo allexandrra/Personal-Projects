144feea4da96cd67bdfad287ccac7a87d3f240c3
Alexandra Gazdaru <alinaalexandragazdaru@gmail.com>
2020-06-07 20:00:00 +0300

    licenta files

---
+++ Licenta/LiveToGo/app/src/main/java/com/example/livetogo/ServerAuthenticator.java
@@ -0,0 +1,17 @@
+package com.example.livetogo;
+
+import java.util.Map;
+
+public class ServerAuthenticator implements IServerAuthenticator {
+    private static Map<String, String> credentialsRepo;
+
+    @Override
+    public String signUp(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public String logIn(String email, String password) {
+        return null;
+    }
+}
---
+++ Licenta/LiveToGo/app/src/main/java/com/example/livetogo/SignupPage.java
@@ -0,0 +1,115 @@
+package com.example.livetogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SignupPage extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+    @BindView(R.id.email) EditText emailText;
+    @BindView(R.id.password) EditText passwordText;
+    @BindView(R.id.re_password) EditText rePasswordText;
+    @BindView(R.id.signupButton) Button signupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_page);
+        ButterKnife.bind(this);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if(!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupPage.this,
+                R.style.Theme_AppCompat_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating account...");
+        progressDialog.show();
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+        String rePassword = rePasswordText.getText().toString();
+
+        // SignUp logic here
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        onSignupSuccess();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    public void onSignupSuccess() {
+        signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+        startActivity(intent);
+        finish();
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Sign Up failed.", Toast.LENGTH_LONG).show();
+        signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+        String rePassword = rePasswordText.getText().toString();
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.setError("Please enter a valid email adress.");
+            valid = false;
+        } else {
+            emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 8) {
+            passwordText.setError("Password must be at least 8 characters long");
+            valid = false;
+        } else {
+            passwordText.setError(null);
+        }
+
+        if(!rePassword.equals(password)) {
+            rePasswordText.setError("Passwords do not match.");
+            valid = false;
+        } else {
+            rePasswordText.setError(null);
+        }
+
+        return valid;
+    }
+}
