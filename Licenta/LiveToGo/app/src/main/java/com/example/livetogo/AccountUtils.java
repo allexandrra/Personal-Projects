58011d78cdade76a8ea39972a777d346beb41426
Alexandra Gazdaru <alinaalexandragazdaru@gmail.com>
2020-06-18 13:33:38 +0300

    xz

--- Licenta/LiveToGo/app/src/main/java/com/example/livetogo/AccountUtils.java
+++
@@ -1,5 +0,0 @@
-package com.example.livetogo;
-
-public class AccountUtils {
-
-}
--- Licenta/LiveToGo/app/src/main/java/com/example/livetogo/IServerAuthenticator.java
+++
@@ -1,6 +0,0 @@
-package com.example.livetogo;
-
-public interface IServerAuthenticator {
-    public String signUp(final String email, final String password);
-    public String logIn(final String email, final String password);
-}
